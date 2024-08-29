a2854519aa209b430d7f9f050132bea7d6d5853d
harshiniakshaya <harshiniakshaya@gmail.com>
2024-08-29 09:21:25 +0530

    project setup

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+const App = () => {
+  return (
+    <div>
+       <h1 className="text-3xl font-bold underline">
+          <Button>Click me</Button>
+        </h1>
+    </div>
+  )
+}
+
+export default App
